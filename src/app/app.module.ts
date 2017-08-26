7eb99b4f3421c3995b49e384bbe933dd6f074727
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-08-26 16:59:00 +0530

    Added two child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EntryListComponent, EntryComponent } from './entries';
+
 
 @NgModule({                      //decorator used for post processor
     imports: [BrowserModule],
-    declarations: [AppComponent],   // Declaration is for registering all of app coponents 
+    declarations: [
+        AppComponent,
+        EntryComponent,         // Always put child component first
+        EntryListComponent  
+    ],   // Declaration is for registering all of app coponents 
     bootstrap: [AppComponent]   // tells compiler to start appcompoent at lauch . A collection of top level components that acts as an entry point of of app.
 
 
